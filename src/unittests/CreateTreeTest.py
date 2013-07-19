422ec91c33d3bff06f82b617b3c678ad086a2376
nodebench <nodebench@gmail.com>
2013-07-19 16:19:43 +0530

    Included utilities.py

--- src/unittests/CreateTreeTest.py
+++ src/unittests/CreateTreeTest.py
@@ -5,19 +5,36 @@ Created on 14-Jul-2013
 
 import unittest
 from engine.treebuilder.Impleme_package import package
+from engine.treebuilder import utilities
+
 
 
 class Test(unittest.TestCase):
 
 
-    def testMakeTree(self):        
+    def test_MakeTree(self):        
         self.assertEqual(str(package) , "<Object Package>" , "Success" )
         
-    def testNewElement(self):        
+    def test_NewElement(self):        
         self.assertEqual(len(package) , 0 , "Success" )        
         self.assertEqual(package.new() , "" , "Success" )
         self.assertEqual(len(package) , 1 , "Success" )
         
+    def test_UtilsRandomnum(self):
+        seqln =5
+        rseq =[ utilities.randseq(seqln) for dummy_i in range(10000) ]             
+        tseq = utilities.randseq(seqln)
+        msg = "Random word '" + str(tseq) + "' repeated."
+        self.assertNotEqual(tseq in rseq ,True ,msg )
+        
+    def test_hashtest(self):
+        seqln =5
+        rseq =[ utilities.genhash(utilities.randseq(seqln) ,seqln) for dummy_i in range(10000) ]
+        tseq = utilities.genhash(utilities.randseq(seqln) ,seqln)
+        msg = "Random word '" + str(tseq) + "' repeated."
+        self.assertNotEqual(tseq in rseq ,True ,msg )
+        
+
         
 
 if __name__ == "__main__":
