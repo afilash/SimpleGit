5cf8fafbb26bda8fa49f978fcbce5bf66d7968b8
nodebench <nodebench@gmail.com>
2013-07-22 16:36:07 +0530

    test preservation check 2.

--- src/engine/Main.py
+++ src/engine/Main.py
@@ -11,7 +11,7 @@ def CreateElementPack():
     print package.delete(elementId=3)
     print package.delete(elementId=1)
     print "checking for preservation added on cba9582437515e1b8e2fec8b25f9b3248a55d837"
-    
+    print "checking for preservation added on 3a7d6c8a31aedafb830daa2914bb0cbb90c0e392"
 
 
 
