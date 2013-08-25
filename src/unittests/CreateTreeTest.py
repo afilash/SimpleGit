9235ce301c398294a157d5faaa24af0e764427e9
nodebench <nodebench@gmail.com>
2013-08-25 16:13:07 +0530

    Changing base from Nodebench to Afilash.

--- src/unittests/CreateTreeTest.py
+++ src/unittests/CreateTreeTest.py
@@ -13,26 +13,29 @@ class Test(unittest.TestCase):
 
 
     def test_MakeTree(self):        
-        self.assertEqual(str(package) , "<Object Package>" , "Success" )
+        self.assertEqual(str(package) , "<Object Package>" , "Success")
         
     def test_NewElement(self):        
-        self.assertEqual(len(package) , 0 , "Success" )        
-        self.assertEqual(package.new() , "" , "Success" )
-        self.assertEqual(len(package) , 1 , "Success" )
+        self.assertEqual(len(package) , 0 , "Success")        
+        self.assertEqual(package.new() , "" , "Success")
+        self.assertEqual(len(package) , 1 , "Success")
         
     def test_UtilsRandomnum(self):
-        seqln =5
-        rseq =[ utilities.randseq(seqln) for dummy_i in range(10000) ]             
+        seqln = 5
+        rseq = [ utilities.randseq(seqln) for dummy_i in range(10000) ]             
         tseq = utilities.randseq(seqln)
         msg = "Random word '" + str(tseq) + "' repeated."
-        self.assertNotEqual(tseq in rseq ,True ,msg )
+        self.assertNotEqual(tseq in rseq , True , msg)
         
     def test_hashtest(self):
-        seqln =5
-        rseq =[ utilities.genhash(utilities.randseq(seqln) ,seqln) for dummy_i in range(10000) ]
-        tseq = utilities.genhash(utilities.randseq(seqln) ,seqln)
+        seqln = 5
+        rseq = [ utilities.genhash(utilities.randseq(seqln) , seqln) for dummy_i in range(10000) ]
+        tseq = utilities.genhash(utilities.randseq(seqln) , seqln)
         msg = "Random word '" + str(tseq) + "' repeated."
-        self.assertNotEqual(tseq in rseq ,True ,msg )
+        self.assertNotEqual(tseq in rseq , True , msg)
+        
+    def test_ispushworking(self):
+        print "OK"
         
 
         
